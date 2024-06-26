369698e4a5e7ea440a49b59a1264288e63ddeddc
Achraf atif <59766059+kommairi@users.noreply.github.com>
2024-06-27 00:18:49 +0100

    finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let text = string;
+  if(num == 0){
+    return "";
+  }
+  else if(num < 0){
+    return "ERROR";
+  }
+  else
+  for(i = 0; i < num - 1; i++){
+    text += string;
+  }
+  return text;
 };
 
 // Do not edit below this line
