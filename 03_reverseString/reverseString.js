180fcaf1bb9f9886c2414fbfe19e1a1585a2f12c
Achraf atif <59766059+kommairi@users.noreply.github.com>
2024-06-27 14:26:00 +0100

    finish exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str){
+  let splitStr = str.split("");
+  let reverseArray = splitStr.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
