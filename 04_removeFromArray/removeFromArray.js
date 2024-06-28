6f4e0811128cf649712b38f73d3d68ddedab7625
Achraf atif <59766059+kommairi@users.noreply.github.com>
2024-06-28 21:40:47 +0100

    finish exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removed, removed_two, removed_three, removed_four) {
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === removed || arr[i] === removed_two || arr[i] === removed_three || arr[i] === removed_four){
+      arr.splice(i,1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
