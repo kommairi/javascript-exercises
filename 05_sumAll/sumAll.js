f211daf44689c683ec5f86159e3beefb40c13a83
Achraf atif <59766059+kommairi@users.noreply.github.com>
2024-07-01 14:21:36 +0100

    finish exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(var_1, var_2) {
+  let sum = 0;
+  if ( var_1 < 0 || var_2 < 0 || isNaN(var_1) || isNaN(var_2) || typeof var_2 !== 'number'){
+    return "ERROR";
+  }
+
+  else if(var_1 >= var_2){
+    for(let i = var_2; i <= var_1; i ++){
+      sum += i;
+    }
+    return sum;
+  }
+  else if(var_2 >= var_1){
+    for(let i = var_1; i <= var_2; i++){
+      sum += i;
+    }
+    return sum;
+  }
+
+
 
 };
 
