1349a147fc0fe21029e89bb379f94fae7b97df02
Achraf atif <59766059+kommairi@users.noreply.github.com>
2024-07-01 16:42:53 +0100

    finish exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedCelsius = ((fahrenheit - 32) * 5) / 9;
+  return Math.round(convertedCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedFahrenheit = ((celsius / 5 ) * 9) + 32;
+  return Math.round(convertedFahrenheit * 10 ) / 10;
+  
 };
 
 // Do not edit below this line
